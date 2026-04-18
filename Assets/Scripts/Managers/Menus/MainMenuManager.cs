0b09a6fd48fc75070eb2c0257990d5be88fb9281
Selimhafez1 <selimhafez7@gmail.com>
2026-04-18 23:01:44 +0200

    Bonus
    Frame Rate 90

--- Assets/Scripts/Managers/Menus/MainMenuManager.cs
+++ Assets/Scripts/Managers/Menus/MainMenuManager.cs
@@ -3,6 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    //to target 90 frames per sec 
+    private void Awake()
+    {
+        Application.targetFrameRate = 90;
+    }
+
     //once player presses the play button, it loads the login scene 
     public void PlayGame()
     {
